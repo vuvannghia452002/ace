aedb7a5d88f4ca565a6db50fcce89930e31f5b75
VuVanNghia <nghiavu2k2abc@gmail.com>
2024-04-02 21:42:13 +0700

    delete 1 file and update 1 file

--- contents/email.py
+++
@@ -1,8 +0,0 @@
-def create_user_name():
-    from faker import Faker
-    fake = Faker()
-    username = fake.user_name() + "@" + fake.user_name() + "." + fake.user_name()
-    return username
-
-
-print("Tên người dùng ngẫu nhiên:", create_user_name())
--- contents/main.py
+++ contents/main.py
@@ -1,75 +1,137 @@
+import random
+import string
 import requests
-import datetime
-from time import sleep
 
 
-url_register = 'https://acenetwork.online/api/v1/passport/auth/register'
-url_login = 'https://acenetwork.online/api/v1/passport/auth/login'
-url_order = 'https://acenetwork.online/api/v1/user/order/save'
-url_checkout = 'https://acenetwork.online/api/v1/user/order/checkout'
+def generate_random_text(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters_and_digits) for _ in range(length))
 
 
+def create_user_name():
+    username = generate_random_text(5)
+    username += "@"
+    username += generate_random_text(5)
+    username += "."
+    username += generate_random_text(5)
+
+    return username
 
 
+def register(username):
+    print("Đăng ký:", end=" ")
 
+    data = {
+        'email': f'{username}',
+        'password': f'{username}'
+    }
 
+    response = requests.post(
+        'https://acenetwork.online/api/v1/passport/auth/register',
+        data=data
+    )
 
-for i in range(2):
-    #! Tạo acc
-    acc = datetime.datetime.now()
-    acc = acc.strftime("%Y_%m_%d_%H_%M_%S_%f")
-    acc = f'{acc}@gmail.com'
-
-    #! Đăng ký
-    data = {'email': f'{acc}', 'password': f'{acc}'}
-
-    response = requests.post(url_register, data=data)
     if response.status_code == 200:
         print("Đăng ký thành công!")
+        response_data = response.json()
+        auth_data = response_data['data']['auth_data']
+        return auth_data
     else:
         print("Đăng ký thất bại. Mã lỗi:", response.status_code)
         exit()
 
-    #! Đặt hàng
-    response_data = response.json()
-    auth_data = response_data['data']['auth_data']
-    data = {"period": "onetime_price",
-            "plan_id": "17",    "coupon_code": "FREE100"}
-    headers = {'Authorization': auth_data}
 
-    response = requests.post(url_order, json=data, headers=headers)
+def order(auth):
+    print("Đặt hàng:", end=" ")
+
+    headers = {'Authorization': auth}
+
+    data = {
+        "period": "onetime_price",
+        "plan_id": "17",
+        "coupon_code": "FREE100"
+    }
+
+    response = requests.post(
+        'https://acenetwork.online/api/v1/user/order/save',
+        json=data,
+        headers=headers
+    )
+
     if response.status_code == 200:
         print("Đặt hàng thành công!")
+        response_data = response.json()
+        invoice = response_data['data']
+        return invoice
     else:
         print("Đặt hàng thất bại. Mã lỗi:", response.status_code)
         exit()
 
-    #! Thanh toán
-    response_data = response.json()
-    invoice = response_data['data']
-    data = {"trade_no": invoice, "method": "1"}
-    headers = {'Authorization': auth_data}
 
-    response = requests.post(url_checkout, json=data, headers=headers)
+def pay(auth, invoice):
+    print("Thanh toán:", end=" ")
+
+    headers = {'Authorization': auth}
+
+    data = {
+        "trade_no": invoice,
+        "method": "1"
+    }
+
+    response = requests.post(
+        'https://acenetwork.online/api/v1/user/order/checkout',
+        json=data,
+        headers=headers
+    )
+
     if response.status_code == 200:
         print("Thanh toán thành công!")
     else:
         print("Thanh toán thất bại. Mã lỗi:", response.status_code)
         exit()
 
-    #! Đăng nhập
-    data = {'email': f'{acc}', 'password': f'{acc}'}
 
-    response = requests.post(url_login, data=data)
+def log_in(username):
+    print("Đăng nhập:", end=" ")
+
+    data = {
+        'email': f'{username}',
+        'password': f'{username}'
+    }
+
+    response = requests.post(
+        'https://acenetwork.online/api/v1/passport/auth/login',
+        data=data
+    )
+
     if response.status_code == 200:
         print("Đăng nhập thành công!")
+        response_data = response.json()
+        token = response_data['data']['token']
+        return token
     else:
         print("Đăng nhập thất bại. Mã lỗi:", response.status_code)
         exit()
-    response_data = response.json()
-    token = response_data['data']['token']
 
+def save_token(username,token):
+    print("Lưu token:", end=" ")
     with open("nghia.txt", "a") as file:
-        file.write(acc + "\n")
+        file.write(username + "\n")
         file.write(
             f"https://acenetwork.online/api/v1/client/subscribe?token={token}" + "\n")
+
+
+def main():
+    try:
+        username = create_user_name()
+        auth = register(username)
+        invoice = order(auth)
+        pay(auth, invoice)
+        token = log_in(username)
+        save_token(username,token)
+    except Exception as e:
+        print("Đã xảy ra một lỗi:", e)
+
+
+if __name__ == "__main__":
+    main()
